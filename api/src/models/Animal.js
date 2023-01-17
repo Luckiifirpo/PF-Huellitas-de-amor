c9542c8b31e8d833e2d638c455fa56797f46066d
NahuK <nahuelgrodz@gmail.com>
2023-01-17 16:38:03 -0300

    id-agregado

--- api/src/models/Animal.js
+++ api/src/models/Animal.js
@@ -4,6 +4,11 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('animal', {
+    id:{
+      type: DataTypes.UUID, 
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
