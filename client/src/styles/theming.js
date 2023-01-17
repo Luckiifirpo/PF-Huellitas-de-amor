c6a235432a712fb928f44ca706cfa468c9f6105e
Lereysis <lereysisquezada@gmail.com>
2023-01-16 23:16:17 -0500

    feature: added vite and created boilerplate initial

---
+++ client/src/styles/theming.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+          main: '#FF3041',
+        },
+        secondary: {
+          main: '#3B57A9',
+        },
+      },
+})
+
+export default theme;
