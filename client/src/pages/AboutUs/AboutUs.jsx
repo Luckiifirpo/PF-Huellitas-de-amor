5107d05ef32dce6c3a660bba3f2c3567d39ee2eb
William Estrada <will.kmiloeo@hotmail.com>
2023-01-18 17:57:58 -0500

    card-components

--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -1,8 +1,26 @@
+import { Grid } from '@mui/material'
+import { Container } from '@mui/system'
 import React from 'react'
+import TeamMemberCard from '../../components/TeamMemberCard/TeamMemberCard'
+
+const testingData = {
+  id: "0",
+  name: "Armando Puertas de las Casas",
+  description: "testing description for this component",
+  img: "5907.jpg"
+}
 
 const AboutUs = () => {
   return (
-    <h1>AboutUs</h1>
+    <div>
+      <Container >
+        <Grid container spacing={2} alignItems="center">
+          <Grid item md={4}>
+            <TeamMemberCard data={testingData} />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
   )
 }
 
