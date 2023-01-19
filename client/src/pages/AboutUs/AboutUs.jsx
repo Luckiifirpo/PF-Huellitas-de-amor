3224905f342b66a242bce2337b88805c2a16041c
Alejandro Palacios <alexpalagomez_1255@hotmail.com>
2023-01-18 20:20:52 -0500

    V1 de page de About us

--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const AboutUs = () => {
   return (
-    <h1>AboutUs</h1>
-  )
-}
+    <>
+      <Container>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item md={3}>
+            <Typography
+              component="h4"
+              variant="h4"
+              sx={{
+                color: "#FF3041",
+                textTransform: "uppercase",
+                fontWeight: "700",
+              }}
+            >
+              Quienes Somos?
+            </Typography>
+            <Typography component="p" sx={{ margin: "10px 0px" }}>
+              Somos estudiantes de Henry cursando la etapa final del bootcamp
+              donde tenemos que desarrollar una aplicación en grupo cumpliendo
+              diferentes objetivos propuestos por el bootcamp. Esta aplicación
+              web tiene como objetivo conectar personas con posibles mascotas en
+              adopción, además de brindar la posibilidad de hacer donaciones
+              para mejorar la calidad de vida de las mascotas.
+            </Typography>
+          </Grid>
+          <Grid Grid item md={9}>
+            
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
