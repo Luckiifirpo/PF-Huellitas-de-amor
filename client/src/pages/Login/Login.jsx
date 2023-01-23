0e38ee5b4314186ac2a021fd862fd7dab85f3336
William Estrada <will.kmiloeo@hotmail.com>
2023-01-23 01:38:41 -0500

    feature: botones de autenticacion de terceros funcionales

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,13 +3,66 @@ import { Container } from "@mui/system";
 import logo from "../../assets/image/logo.svg";
 import login_img from "../../assets/image/login-img.png";
 import style from "./Login.module.css";
-import GoogleIcon from '@mui/icons-material/Google';
+import { MicrosoftLoginButton, GoogleLoginButton, GithubLoginButton } from "react-social-login-buttons";
 import { Link } from "react-router-dom";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, OAuthProvider, signInWithPopup } from "firebase/auth";
+import FirebaseApp from "../../services/firebaseApp";
 
 const Login = (props) => {
+
+    const firebaseAuth = getAuth(FirebaseApp);
+    firebaseAuth.languageCode = 'es';
+
+    const googleAuthProvider = new GoogleAuthProvider();
+    const githubAuthProvider = new GithubAuthProvider();
+    const microsoftAuthProvider = new OAuthProvider("microsoft.com");
+
+
+    const loginWithGoogle = () => {
+        signInWithPopup(firebaseAuth, googleAuthProvider)
+            .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
+    }
+
+    const loginWithGithub = () => {
+        signInWithPopup(firebaseAuth, githubAuthProvider)
+            .then((result) => {
+                const credential = GithubAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = GithubAuthProvider.credentialFromError(error);
+            });
+    }
+
+    const loginWithMicrosoft = () => {
+        signInWithPopup(firebaseAuth, microsoftAuthProvider)
+            .then((result) => {
+                const credential = OAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = OAuthProvider.credentialFromError(error);
+            });
+    }
+
     return (
         <div className={style.login_div}>
-            <Container style={{minHeight:"100vh", display: "flex"}}>
+            <Container style={{ minHeight: "100vh", display: "flex" }}>
                 <Grid container spacing={4} alignItems="center">
                     <Grid item lg={7}>
                         <img src={login_img} />
@@ -18,7 +71,7 @@ const Login = (props) => {
 
                     </Grid>
                     <Grid item lg={4}>
-                        <Paper style={{paddingBottom: 20}}>
+                        <Paper style={{ paddingBottom: 20 }}>
                             <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
                                 <Grid item>
                                     <img className={style.logo} src={logo} />
@@ -45,14 +98,15 @@ const Login = (props) => {
                                 </Grid>
                                 <Grid item>
                                     <ButtonGroup orientation="vertical">
-                                        <Button color="secondary" variant="outlined" className={style.input_width + " " + style.auth_button}>
-                                            <GoogleIcon style={{ marginRight: 10 }} />
-                                            <span>Continuar con Dropbox</span>
-                                        </Button>
-                                        <Button color="secondary" variant="outlined" className={style.input_width + " " + style.auth_button}>
-                                            <GoogleIcon style={{ marginRight: 10 }} />
+                                        <GithubLoginButton iconSize="16px" onClick={loginWithGithub} color="secondary" variant="outlined" className={style.input_width + " " + style.auth_button}>
+                                            <span>Continuar con GitHub</span>
+                                        </GithubLoginButton>
+                                        <GoogleLoginButton iconSize="16px" onClick={loginWithGoogle} color="secondary" variant="outlined" className={style.input_width + " " + style.auth_button}>
                                             <span>Continuar con Google</span>
-                                        </Button>
+                                        </GoogleLoginButton>
+                                        <MicrosoftLoginButton iconSize="16px" onClick={loginWithMicrosoft} color="secondary" variant="outlined" className={style.input_width + " " + style.auth_button}>
+                                            <span>Continuar con Microsoft</span>
+                                        </MicrosoftLoginButton>
                                     </ButtonGroup>
                                 </Grid>
                                 <Grid item>
