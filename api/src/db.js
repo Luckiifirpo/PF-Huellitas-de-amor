81673606c4952799f2d35493f341dee3ef6fcf6c
wal90 <walteromartinez90@gmail.com>
2023-01-25 13:42:25 -0300

    cambio en el archivo db.js

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://postgres:36520837@localhost/huellitas`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
