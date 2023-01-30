677e3b66cab4407b4876967c9ad121123fa4ab23
Oskarp88 <oskyburgos81@gmail.com>
2023-01-30 13:32:53 -0500

    olvido contraseña y recuperando contraseña

--- client/src/pages/ForgotPassword/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -33,29 +33,33 @@ import style from './ForgotPassword.module.css';
         return(
          <div className={style.login_div}>
             <Container style={{ minHeight: "100vh", display: "flex" }}>
-               
-            <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
+               <Grid container spacing={4} alignItems="center">
+                 <Grid item lg={4}>
+                    <Paper style={{ paddingBottom: 20 }}>
+                        <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
+                            <Grid item>
+                                <img className={style.logo} src={logo} />
+                            </Grid>
+                            <Grid item>
+                                <Typography component="h1" sx={{ color: '#FF3041', fontWeight: 'Bold', marginLeft: '10px' }}>
+                                    {"Ingresa tu correo electrónico para buscar tu cuenta."}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <form onSubmit={userForgotPassword}>
+                            <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
                                 <Grid item>
-                                    <img className={style.logo} src={logo} />
+                                  <TextField type="email" size="small" id="email-input" label={"ingresa tu email"} variant="standard" className={style.input_width} />
                                 </Grid>
+                                    
                                 <Grid item>
-                                    <Typography component="h1" sx={{ color: '#FF3041', fontWeight: 'Bold' }}>
-                                        {"Ingresa tu correo electrónico para buscar tu cuenta."}
-                                    </Typography>
+                                   <Button type="submit" variant="contained" color='info' size="medium" sx={{ borderRadius: '20px' }} className={style.input_width}>{"Enviar"}</Button>
                                 </Grid>
                             </Grid>
-                            <form onSubmit={userForgotPassword}>
-                                <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
-                                    <Grid item>
-                                        <TextField type="email" size="small" id="email-input" label={"ingresa tu email"} variant="standard" className={style.input_width} />
-                                    </Grid>
-                                    
-                                    <Grid item>
-                                        <Button type="submit" variant="contained" color='info' size="medium" sx={{ borderRadius: '20px' }} className={style.input_width}>{"Enviar"}</Button>
-                                    </Grid>
-                                </Grid>
-                            </form>
-                            
+                        </form>
+                    </Paper> 
+                  </Grid>   
+                </Grid> 
             </Container>
         </div>
     );
--- client/src/pages/ResetPassword/ResetPassword.jsx
+++ client/src/pages/ResetPassword/ResetPassword.jsx
@@ -4,7 +4,7 @@ import { Container } from "@mui/system";
 import logo from "../../assets/image/logo.svg";
 import { Link, useLocation, useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-
+import login_img from "../../assets/image/login-img.png";
 import { postForgotPassword } from "../../redux/slices/userSlice";
 import style from './ResetPassword.module.css';
 import { useState } from "react";
@@ -20,9 +20,9 @@ const ResetPassword = () => {
        const PasswordInput = e.target.querySelector("#password-input");
        const RePasswordInput = e.target.querySelector("#repassword-input");
        console.log(PasswordInput.value + " contraseña 2 " + RePasswordInput.value);
-    //    if(PasswordInput !== RePasswordInput){
-    //     return alert("contraseña mal escrita");
-    //    }
+       if(PasswordInput !== RePasswordInput){
+        return alert("contraseña mal escrita");
+       }
 
        dispatch(PutresetPassword(id, PasswordInput.value));
     }
@@ -30,8 +30,16 @@ const ResetPassword = () => {
      return(
      <div className={style.login_div}>
         <Container style={{ minHeight: "100vh", display: "flex" }}>
-           
-        <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
+            <Grid container spacing={4} alignItems="center">
+                <Grid item lg={7}>
+                   <img src={login_img} />
+                </Grid>
+                <Grid item lg={1}>
+
+                </Grid>
+                <Grid item lg={4}>
+                    <Paper style={{ paddingBottom: 20 }}>  
+                        <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
                             <Grid item>
                                 <img className={style.logo} src={logo} />
                             </Grid>
@@ -42,25 +50,24 @@ const ResetPassword = () => {
                             </Grid>
                         </Grid>
                         <form onSubmit={userResetPassword}>
-                            <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
-                            <Box component="form" sx={{display:'flex',flexDirection:'column', gap:'15px', justifyContent:'center',height:'100%', margin:'0px 70px'}}>
-                                <Grid item>
-                                
+                            <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>                         
+                                <Grid item>                               
                                    <TextField size="small" id="password-input" type="password" align='center'
-                                      label={"Nueva contraseña"} variant="standard" className={style.input_width} />
-                                
+                                      label={"Nueva contraseña"} variant="standard" className={style.input_width} />                                
                                 </Grid>
                                 <Grid item>
-                                 <TextField size="small" id="repassword-input" type="password" align='center'
+                                   <TextField size="small" id="repassword-input" type="password" align='center'
                                       label={"Repite la contraseña"} variant="standard" className={style.input_width} />
                                 </Grid>
-                                </Box>
+                                
                                 <Grid item>
                                     <Button type="submit" variant="contained" color='info' size="medium" sx={{ borderRadius: '20px' }} className={style.input_width}>{"Cambiar contraseña"}</Button>
                                 </Grid>
                             </Grid>
                         </form>
-                        
+                    </Paper> 
+                </Grid>
+            </Grid>  
         </Container>
     </div>
 );
