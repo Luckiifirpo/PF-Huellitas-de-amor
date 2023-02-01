639c9ad0d81d398dfa7bea7cbb2e16ff1f4a7301
Luciano Firpo <lucianofirpopp@gmail.com>
2023-02-01 14:20:18 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
+require("dotenv").config()
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const {PORT} = process.env
 // Syncing all the models at once.
 
 
 
 conn.sync({ force: true }).then(() => {
 
-  server.listen(3001, () => {
-    console.log('server listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`server listening at ${PORT}`) // eslint-disable-line no-console
   });
 });
