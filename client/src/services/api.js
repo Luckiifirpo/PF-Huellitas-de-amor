17ec4e82889fb02ac3ea97e7164722a519aa28d6
Luciano Firpo <lucianofirpopp@gmail.com>
2023-02-01 14:38:01 -0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://huellitas-de-amor-server.onrender.com/',
+//     baseURL: 'https://huellitas-de-amor-server.onrender.com/',
+       baseURL: `https://pf-huellitas-de-amor-production.up.railway.app/`,
 })
 
 export default api
