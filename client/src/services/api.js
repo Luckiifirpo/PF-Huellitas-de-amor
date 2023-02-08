e593241afb816a0a105eec270b7d8989118979db
wal90 <walteromartinez90@gmail.com>
2023-02-08 15:47:54 -0300

    cambio de posicion del logo en el navbar

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-       // baseURL: `https://pf-huellitas-de-amor-production.up.railway.app/`,
-       baseURL: "http://localhost:3001/"
+       baseURL: `https://pf-huellitas-de-amor-production.up.railway.app/`,
+       //baseURL: "http://localhost:3001/"
 })
 
 export default api
