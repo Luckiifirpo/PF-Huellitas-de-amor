f28e912ef3b5fed1539f55570c346b097da07c1a
Luciano Firpo <lucianofirpopp@gmail.com>
2023-02-09 00:26:02 -0300

    Update DataTablePets.jsx

--- client/src/components/DataTable/DataTablePets.jsx
+++ client/src/components/DataTable/DataTablePets.jsx
@@ -116,7 +116,7 @@ export default function DataTablePets() {
 
     },
     {
-      field: 'IsAdopted',
+      field: 'isAdopted',
       headerName: 'Adoptado',
       type: 'boolean',
       editable: true,
