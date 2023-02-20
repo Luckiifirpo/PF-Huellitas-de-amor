947357c297c02bcc2f0e4169e90b0a12217c04ab
luckiifirpo <lucianofirpopp@gmail.com>
2023-02-20 19:14:39 -0300

    ahora si lo solucione, o eso creo :(

--- api/src/config/firebase-config.js
+++ api/src/config/firebase-config.js
@@ -1,8 +1,8 @@
 var firebaseAdmin = require("firebase-admin");
-var serviceAccount = require("./ServiceAccount.json");
+var service = JSON.parse(process.env.SERVICE_ACCOUNT)
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount)
+  credential: firebaseAdmin.credential.cert(service)
 });
 
 module.exports = firebaseAdmin;
--- client/src/services/firebaseApp.js
+++ client/src/services/firebaseApp.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 // console.log(import.meta.env)
-const { APPID_FIREBASE, MESSAGINGSENDERID_FIREBASE, STORAGEBUCKET_FIREBASE, PROJECTID_FIREBASE,
-  DATABASEURL_FIREBASE, AUTHDOMAIN_FIREBASE, APIKEY_FIREBASE } = import.meta.env
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: APIKEY_FIREBASE,
-  authDomain: AUTHDOMAIN_FIREBASE,
-  databaseURL: DATABASEURL_FIREBASE,
-  projectId: PROJECTID_FIREBASE,
-  storageBucket: STORAGEBUCKET_FIREBASE,
-  messagingSenderId: MESSAGINGSENDERID_FIREBASE,
-  appId: APPID_FIREBASE
+  apiKey: "AIzaSyB2h7PT1yEFGAq8__qWBTfFHGw7EeQ81Iw",
+  authDomain: "huellitas-de-amor-auth.firebaseapp.com",
+  databaseURL: "https://huellitas-de-amor-auth-default-rtdb.firebaseio.com",
+  projectId: "huellitas-de-amor-auth",
+  storageBucket: "huellitas-de-amor-auth.appspot.com",
+  messagingSenderId: "56874258349",
+  appId: "1:56874258349:web:3bad25231714fa64799b24"
 };
 
 // Initialize Firebase
